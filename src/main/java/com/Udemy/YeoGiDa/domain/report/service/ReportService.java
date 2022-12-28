9a8b193b5d1325b4b7adbb83d9e1e043b78e4432
HyeongRae Jo <whgudfo77@gmail.com>
2022-12-28 21:44:40 +0900

    [FEAT] Report, Slack API 개발
    #117

--- src/main/java/com/Udemy/YeoGiDa/domain/report/service/ReportService.java
+++ src/main/java/com/Udemy/YeoGiDa/domain/report/service/ReportService.java
@@ -1,22 +1,15 @@
 package com.Udemy.YeoGiDa.domain.report.service;
 
-import com.Udemy.YeoGiDa.domain.comment.entity.Comment;
-import com.Udemy.YeoGiDa.domain.comment.exception.CommentNotFoundException;
-import com.Udemy.YeoGiDa.domain.comment.repository.CommentRepository;
 import com.Udemy.YeoGiDa.domain.member.entity.Member;
-import com.Udemy.YeoGiDa.domain.member.exception.MemberNotFoundException;
-import com.Udemy.YeoGiDa.domain.member.repository.MemberRepository;
-import com.Udemy.YeoGiDa.domain.place.entity.Place;
-import com.Udemy.YeoGiDa.domain.place.repository.PlaceRepository;
 import com.Udemy.YeoGiDa.domain.report.entity.Report;
 import com.Udemy.YeoGiDa.domain.report.repository.ReportRepository;
-import com.Udemy.YeoGiDa.domain.trip.repository.TripRepository;
-import com.Udemy.YeoGiDa.global.slack.service.SlackSender;
+import com.Udemy.YeoGiDa.domain.report.request.ReportRequestDto;
+import com.Udemy.YeoGiDa.global.slack.service.SlackService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
+import java.io.IOException;
 
 @Service
 @Transactional
@@ -24,51 +17,52 @@ import java.util.List;
 public class ReportService {
 
     private final ReportRepository reportRepository;
-    private final MemberRepository memberRepository;
-    private final TripRepository tripRepository;
-    private final PlaceRepository placeRepository;
-    private final CommentRepository commentRepository;
-    private final SlackSender slackSender;
+    private final SlackService slackService;
 
-    public ReportService(ReportRepository reportRepository, MemberRepository memberRepository,
-                         TripRepository tripRepository, PlaceRepository placeRepository,
-                         CommentRepository commentRepository, SlackSender slackSender) {
+    public ReportService(ReportRepository reportRepository, SlackService slackService) {
         this.reportRepository = reportRepository;
-        this.memberRepository = memberRepository;
-        this.tripRepository = tripRepository;
-        this.placeRepository = placeRepository;
-        this.commentRepository = commentRepository;
-        this.slackSender = slackSender;
+        this.slackService = slackService;
     }
 
-    public void report(Member member, String type, Long targetId) {
-        reportRepository.save(new Report(member, type, targetId));
+//    public void report(Member member, String type, Long targetId) {
+//        reportRepository.save(new Report(member, type, targetId));
+//
+//        StringBuilder sb = new StringBuilder();
+//        String message = "";
+//        if (type.equals("MEMBER")) {
+//            Member findMember = memberRepository.findById(targetId).orElseThrow(MemberNotFoundException::new);
+//            sb.append(":alert:[REPORT]").append(System.getProperty("line.separator"))
+//                    .append("[Member_ID] : ").append(findMember.getId()).append(System.getProperty("line.separator"))
+//                    .append("[Nickname] : ").append(findMember.getNickname()).append(System.getProperty("line.separator"))
+//                    .append("[ImgUrl] : ").append(findMember.getMemberImg().getImgUrl());
+//        } else if (type.equals("TRIP")) {
+//            Trip trip = tripRepository.findById(targetId).orElseThrow(TripNotFoundException::new);
+//            sb.append(":alert:[REPORT]").append(System.getProperty("line.separator"));
+//                sb.append("[Trip_ID] : ").append(trip.getId()).append(System.getProperty("line.separator"))
+//                        .append("[Trip Title] : ").append(trip.getTitle()).append(System.getProperty("line.separator"))
+//                        .append("[Trip SubTitle] : ").append(trip.getSubTitle()).append(System.getProperty("line.separator"))
+//                        .append("[Trip ImgUrl] : ").append(trip.getTripImg().getImgUrl()).append(System.getProperty("line.separator"));
+//        } else if (type.equals("PLACE")) {
+//            Place place = placeRepository.findById(targetId).orElseThrow(PlaceNotFoundException::new);
+//            sb.append(":alert:[REPORT]").append(System.getProperty("line.separator"));
+//                sb.append("[Place_ID] : ").append(place.getId()).append(System.getProperty("line.separator"))
+//                        .append("[Place Title] : ").append(place.getTitle()).append(System.getProperty("line.separator"))
+//                        .append("[Place Content] : ").append(place.getContent()).append(System.getProperty("line.separator"))
+//                        .append("[Place ImgUrl] : ").append(place.getPlaceImgs().get(0).getImgUrl()).append(System.getProperty("line.separator"));
+//        } else if (type.equals("COMMENT")) {
+//            Comment comment = commentRepository.findById(targetId).orElseThrow(CommentNotFoundException::new);
+//            sb.append(":alert:[REPORT]").append(System.getProperty("line.separator"))
+//                    .append("[Comment_ID] : ").append(comment.getId()).append(System.getProperty("line.separator"))
+//                    .append("[CONTENT] : ").append(comment.getContent()).append(System.getProperty("line.separator"));
+//        }
+//        message = sb.toString();
+//        slackSender.sendSlack(message);
+//    }
 
-        StringBuilder sb = new StringBuilder();
-        String message = "";
-        if(type.equals("MEMBER")) {
-            Member findMember = memberRepository.findById(targetId).orElseThrow(MemberNotFoundException::new);
-            sb.append("[REPORT]").append(System.getProperty("line.separator"))
-                    .append("[Member_ID] : ").append(findMember.getId()).append(System.getProperty("line.separator"))
-                    .append("[Nickname] : ").append(findMember.getNickname()).append(System.getProperty("line.separator"))
-                    .append("[ImgUrl] : ").append(findMember.getMemberImg().getImgUrl());
-        } else if(type.equals("TRIP")) {
-            List<Place> placeList = placeRepository.findAllByTripId(targetId);
-            sb.append("[REPORT]").append(System.getProperty("line.separator"));
-            for (Place place : placeList) {
-                sb.append("[Place_ID] : ").append(place.getId()).append(System.getProperty("line.separator"))
-                        .append("[Place Title] : ").append(place.getTitle()).append(System.getProperty("line.separator"))
-                        .append("[Place Content] : ").append(place.getContent()).append(System.getProperty("line.separator"))
-                        .append("[Place ImgUrl] : ").append(place.getPlaceImgs().get(0).getImgUrl()).append(System.getProperty("line.separator"));
-            }
-        } else if(type.equals("COMMENT")) {
-            Comment comment = commentRepository.findById(targetId).orElseThrow(CommentNotFoundException::new);
-            sb.append("[REPORT]").append(System.getProperty("line.separator"))
-                    .append("[Comment_ID] : ").append(comment.getId()).append(System.getProperty("line.separator"))
-                    .append("[CONTENT] : ").append(comment.getContent()).append(System.getProperty("line.separator"));
-        }
-        message = sb.toString();
-        slackSender.sendSlack(message);
-        log.debug("message={}", message);
+    public void report(Member member, ReportRequestDto reportRequestDto) throws IOException {
+        reportRepository.save(new Report(member, reportRequestDto.getType(), reportRequestDto.getTargetId()));
+
+        slackService.stackBlock(reportRequestDto);
     }
+
 }
