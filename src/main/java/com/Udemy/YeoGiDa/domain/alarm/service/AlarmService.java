14b3f1b9322b9a6b37800885a4500ad140862bb1
HyeongRae Jo <whgudfo77@gmail.com>
2023-01-10 15:38:44 +0900

    [FEAT] 알림목록에서 일주일 지난 알림 삭제 #169

--- src/main/java/com/Udemy/YeoGiDa/domain/alarm/service/AlarmService.java
+++ src/main/java/com/Udemy/YeoGiDa/domain/alarm/service/AlarmService.java
@@ -7,9 +7,11 @@ import com.Udemy.YeoGiDa.domain.member.entity.Member;
 import com.Udemy.YeoGiDa.domain.member.exception.MemberNotFoundException;
 import com.Udemy.YeoGiDa.domain.member.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
@@ -17,6 +19,7 @@ import java.util.List;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class AlarmService {
 
     private final AlarmRepository alarmRepository;
@@ -37,5 +40,16 @@ public class AlarmService {
         }
         Collections.reverse(alarmListResponseDtos);
         return alarmListResponseDtos;
+    }
+
+    public void deleteAlarmsAfterOneWeek() {
+        List<Alarm> all = alarmRepository.findAll();
+        LocalDateTime now = LocalDateTime.now();
+        for (Alarm alarm : all) {
+            LocalDateTime createdTime = alarm.getCreatedTime();
+            if(createdTime.plusDays(7).isBefore(now)) {
+                alarmRepository.delete(alarm);
+            }
+        }
     }
 }
